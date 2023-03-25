fd4a2eb13e27a8e2f5a558c1cf56540a2df8f25f
seveneleven337 <solidxnake_6@hotmail.com>
2023-03-25 20:05:10 +0200

    deploy

--- src/socketPildoras/Socket_server.java
+++ src/socketPildoras/Socket_server.java
@@ -11,22 +11,16 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class Socket_server implements Runnable {
-	
+
 	private static int port = 6000;
 	DataInputStream in;
 	int read = 0;
 	byte[] buffer = new byte[1024];
 	static ServerSocket server;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			server = new ServerSocket(port);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}   //server socket listening in the specified port
 
+	public static void main(String[] args) throws IOException {
+		
+		server = new ServerSocket(port);  // server socket listening in the specified port
 		Runnable runnable = new Socket_server();
 		Thread thread = new Thread(runnable);
 		thread.start();
@@ -36,24 +30,23 @@ public class Socket_server implements Runnable {
 	@Override
 	public void run() {
 		try {
-			
+
 			System.out.println("server running");
-			 while(true){
-			Socket socket = server.accept();				//accept connection from outside			
-			in = new DataInputStream(socket.getInputStream());    //receive an stream from the socket input stream
-			while ((read = in.read(buffer, 0, buffer.length)) != -1) {
-		        in.read(buffer);
-		    }
-			System.out.println(new String(buffer, StandardCharsets.UTF_8));
-			socket.close();
-			buffer = new byte[1024];
-			 }	
-			 
-		}	
-		
-		catch (EOFException e) {
+			while (true) {
+				Socket socket = server.accept(); // accept connection from outside
+				in = new DataInputStream(socket.getInputStream()); // receive an stream from the socket input stream
+				while ((read = in.read(buffer, 0, buffer.length)) != -1) {
+					in.read(buffer);
+				}
+				System.out.println(socket.getInetAddress().toString());
+				System.out.println(new String(buffer, StandardCharsets.UTF_8));
+				socket.close();
+				buffer = new byte[1024];
+			}
+
+		} catch (EOFException e) {
 			System.out.println(e);
-				
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
